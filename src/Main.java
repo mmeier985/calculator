92887c2bcb8ddc1a32fd71cfbcc4b6f22ea1175e
Mason Meier <masonmeier13@gmail.com>
2025-07-20 20:09:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 2 numbers and operation:");
+        String inputNum1 = input.nextLine().toLowerCase();
+        char opera = input.next().charAt(0);
+        String inputNum2 = input.nextLine().toLowerCase();
+        input.close();
+
+        BigDecimal pi = BigDecimal.valueOf(Math.PI);
+        BigDecimal num1, num2;
+
+        if (inputNum1.equals("pi")){
+            num1 = pi;
+        } else {
+            num1 = BigDecimal.valueOf(Double.parseDouble(inputNum1));
+        }
+
+        if (inputNum2.equals("pi")){
+            num2 = pi;
+        } else {
+            num2 = BigDecimal.valueOf(Double.parseDouble(inputNum2));
+        }
+
+
+        if (opera == '+') {
+            System.out.println(num1 + num2);
+
+        } else if (opera == '-') {
+            System.out.println(num1 - num2);
+
+        } else if (opera == '/') {
+            System.out.println(num1 / num2);
+
+        } else if (opera == '*') {
+            System.out.println(num1 * num2);
+
+        } else {
+            System.out.println("Invalid Input, please try again.");
+        }
+    }
+}
